b0e5e5b866bc1aeb48b3122fca658728cb9c3376
Alfaza Satria <alfazasatria8@gmail.com>
2020-10-04 22:15:05 +0700

    add schema and model Siswa

---
+++ database/migrations/2020_10_04_151318_create_table_siswa.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableSiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->string('nisn')->primary();
+            $table->string('nama_siswa');
+            $table->string('email');
+            $table->string('password');
+            $table->string('kode_kelas');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
