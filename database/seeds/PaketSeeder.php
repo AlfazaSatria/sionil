958fe31b45f9aec12355f793c0968e9ea204ff4f
Alfaza Satria <alfazasatria8@gmail.com>
2020-10-26 02:19:56 +0700

    Edit Seeder

--- database/seeds/PaketSeeder.php
+++ database/seeds/PaketSeeder.php
@@ -14,51 +14,24 @@ class PaketSeeder extends Seeder
     {
         DB::table('paket')->insert([
             'id' => 1,
-            'ket' => 'Bisnis kontruksi dan Properti',
+            'ket' => 'English',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
+
         DB::table('paket')->insert([
             'id' => 2,
-            'ket' => 'Desain Permodelan dan Informasi Bangunan',
+            'ket' => 'Pendidikkan Agama Islam',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
+
         DB::table('paket')->insert([
             'id' => 3,
-            'ket' => 'Elektronika Industri',
+            'ket' => 'Tahfizh',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
-        DB::table('paket')->insert([
-            'id' => 4,
-            'ket' => 'Otomasi Industri',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s')
-        ]);
-        DB::table('paket')->insert([
-            'id' => 5,
-            'ket' => "Teknik Pemesinan",
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s')
-        ]);
-        DB::table('paket')->insert([
-            'id' => 6,
-            'ket' => 'Teknik dan Bisnis Sepeda Motor',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s')
-        ]);
-        DB::table('paket')->insert([
-            'id' => 7,
-            'ket' => 'Rekayasa Perangkat Lunak',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s')
-        ]);
-        DB::table('paket')->insert([
-            'id' => 8,
-            'ket' => "Teknik Pengelasan",
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s')
-        ]);
+       
     }
 }
--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -17,7 +17,7 @@ class UsersSeeder extends Seeder
             'id' => 1,
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('12345678'),
+            'password' => Hash::make('admin123'),
             'role' => 'Admin',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
