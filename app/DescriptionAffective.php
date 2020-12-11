1ca9b53dcedadb0b77bdd195cb8627cf9bb11057
Alfaza Satria <alfazasatria8@gmail.com>
2020-12-11 12:26:49 +0700

    Debug

--- app/DescriptionAffective.php
+++ app/DescriptionAffective.php
@@ -9,9 +9,13 @@ class DescriptionAffective extends Model
     protected $table = 'description_affective';
 
     protected $fillable = [
-        'deskripsi_a',
-        'deskripsi_b',
-        'deskripsi_c',
-        'deskripsi_d'
+        'deskripsi_a_sp',
+        'deskripsi_b_sp',
+        'deskripsi_c_sp',
+        'deskripsi_d_sp',
+        'deskripsi_a_so',
+        'deskripsi_b_so',
+        'deskripsi_c_so',
+        'deskripsi_d_so',
     ];
 }
